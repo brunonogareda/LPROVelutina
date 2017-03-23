768de3390966d157b5360883d6df7662f9825e55
Waheedmm <wdmdml@gmail.com>
2017-03-23 11:51:37 +0100

    pruebas

--- main.cpp
+++ main.cpp
@@ -13,94 +13,94 @@
  using namespace std;
  using namespace cv;
 
-int main( int argc, char** argv )
-{
+//int main( int argc, char** argv )
+//{
 
-       //Para vídeo o cámara por defecto
-       std::string arg = argv[1];
+//       //Para vídeo o cámara por defecto
+//       std::string arg = argv[1];
 
-   VideoCapture cap(arg); //capture the video from web cam
+//   VideoCapture cap(arg); //capture the video from web cam
 
-   // Si falla, activa la cámara por defecto
-    if (!cap.isOpened())
-        cap.open(atoi(arg.c_str()));
-    if (!cap.isOpened()) {
-        cerr << "No se ha podido abrir el archivo ni activar la cámara\n" << endl;
-        return 1;
-    }
+//   // Si falla, activa la cámara por defecto
+//    if (!cap.isOpened())
+//        cap.open(atoi(arg.c_str()));
+//    if (!cap.isOpened()) {
+//        cerr << "No se ha podido abrir el archivo ni activar la cámara\n" << endl;
+//        return 1;
+//    }
 
-   namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+//   namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
 
- int iLowH = 0;
-int iHighH = 255;
+// int iLowH = 0;
+//int iHighH = 255;
 
- int iLowS = 0;
-int iHighS = 255;
+// int iLowS = 0;
+//int iHighS = 255;
 
- int iLowV = 0;
-int iHighV = 255;
+// int iLowV = 0;
+//int iHighV = 255;
 
- //Create trackbars in "Control" window
-cvCreateTrackbar("LowH", "Control", &iLowH, 255); //Hue (0 - 179)
-cvCreateTrackbar("HighH", "Control", &iHighH, 255);
+// //Create trackbars in "Control" window
+//cvCreateTrackbar("LowH", "Control", &iLowH, 255); //Hue (0 - 179)
+//cvCreateTrackbar("HighH", "Control", &iHighH, 255);
 
- cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
-cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+// cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+//cvCreateTrackbar("HighS", "Control", &iHighS, 255);
 
- cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
-cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+// cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+//cvCreateTrackbar("HighV", "Control", &iHighV, 255);
 
-   while (true)
-   {
-       Mat imgOriginal;
+//   while (true)
+//   {
+//       Mat imgOriginal;
 
-       bool bSuccess = cap.read(imgOriginal); // read a new frame from video
+//       bool bSuccess = cap.read(imgOriginal); // read a new frame from video
 
-        if (!bSuccess) //if not success, break loop
-       {
-            cout << "Cannot read a frame from video stream" << endl;
-            break;
-       }
+//        if (!bSuccess) //if not success, break loop
+//       {
+//            cout << "Cannot read a frame from video stream" << endl;
+//            break;
+//       }
 
-   Mat imgHSV;
+//   Mat imgHSV;
 
-  cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+//  cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
 
- Mat imgThresholded;
+// Mat imgThresholded;
 
-  inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+//  inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
 
- //morphological opening (remove small objects from the foreground)
- erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
- dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+// //morphological opening (remove small objects from the foreground)
+// erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+// dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
 
-  //morphological closing (fill small holes in the foreground)
- dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
- erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+//  //morphological closing (fill small holes in the foreground)
+// dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+// erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
 
-  imshow("Thresholded Image", imgThresholded); //show the thresholded image
- imshow("Original", imgOriginal); //show the original image
- waitKey();
+//  imshow("Thresholded Image", imgThresholded); //show the thresholded image
+// imshow("Original", imgOriginal); //show the original image
+// waitKey();
 
-       if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-      {
-           cout << "esc key is pressed by user" << endl;
-           break;
-      }
+//       if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
+//      {
+//           cout << "esc key is pressed by user" << endl;
+//           break;
+//      }
 
-   }
+//   }
 
-  return 0;
+//  return 0;
 
-}
-
-
+//}
 
 
 
 
 
 
+
+dfsafdsfsafsfsfsfsfsfsfsfsafsfsa
 
 
 
