e50e74cf4083d1e1b30c1f8376bc4b7d4713e61a
Crowsler <xaimino_64@hotmail.es>
2017-03-29 11:30:55 +0200

    Main detect fondo
    Añadida la fusión de recuadros superpuestos

--- main.cpp
+++ main.cpp
@@ -17,7 +17,9 @@
 Mat detectForeground( Mat frame , Mat& Mask , BackgroundSubtractorMOG2& bgModel);
 vector<Rect> detecta_contorno(Mat mascara, Mat frame);
 vector<Point2i> histograma(vector<Rect> boundRect, Mat frame);
-Point2i angulo(vector<Rect> boundRect, int i, Mat frame);
+vector<Rect> comp_Rect(vector<Rect> amarillo, Rect boundRect);
+vector<Point2i> hist_no_mov(Mat frame);
+Point2i angulo(Rect coord, Mat frame);
 void sliders(Mat mascara);
 
 /** Variables globales */
@@ -74,19 +76,20 @@ Mat descriptorsFace, descriptorsFrame;
    // Usado para guardar imágenes
    int n = 0;
    char filename[200];
-
    for (;;) {
        capture >> frame;
        if( !frame.empty() )
        {
+           //Con detect foreground
+           //mascara=detectForeground( frame , Mask , bgModel);
+           //boundRect=detecta_contorno(mascara==255, frame);
+           //puntos=histograma(boundRect, frame);
 
-           mascara=detectForeground( frame , Mask , bgModel);
-           //Reduce el tamaño de ventana
-           boundRect=detecta_contorno(mascara==255, frame);
-           puntos=histograma(boundRect, frame);
-           Point2i punto = puntos[0];
-           system("python servo_coords.py "+punto.x + " " +punto.y);
+           //Sin detect foreground
+           puntos=hist_no_mov(frame);
+
            resize(frame, original, Size(), 0.5, 0.5, INTER_LINEAR);
+
        }
        else
        { printf(" --(!) No captured frame -- Break!"); break; }
@@ -109,8 +112,7 @@ Mat descriptorsFace, descriptorsFrame;
        case 'l':
             sliders(frame);
        case 'c':
-            cout << "Tamano: " << puntos.size() << endl;
-            cout << "Coordenadas obtenidas: " << puntos[0] << endl;
+            cout << "Coordenadas obtenidas: " << puntos << endl;
             waitKey();
        default:
             break;
@@ -176,7 +178,7 @@ Mat descriptorsFace, descriptorsFrame;
      for(int i=0; i<contours.size(); i++){
          approxPolyDP(Mat(contours[i]), contours_poly[i], 3, true);
          boundRect[i] = boundingRect(Mat(contours_poly[i]));
-         rectangle(frame, boundRect[i], 100, 2, 8, 0);
+         //rectangle(frame, boundRect[i], 100, 2, 8, 0);
      }
      //Reduce tamaño de ventana
      resize(frame, frameAux, Size(), 0.5, 0.5, INTER_LINEAR);
@@ -185,12 +187,13 @@ Mat descriptorsFace, descriptorsFrame;
  }
 
 vector<Point2i> histograma(vector<Rect> boundRect, Mat frame){
-    vector<Mat> rectInterestedVector;
+
     Mat frameAux;
     Mat rectSection;
     Mat rectSectionHSV;
     Mat rectInterested;
     vector<vector<Point> > contours;
+    vector<Rect> amarillo;
     Point2i punto;
     vector<Point2i> puntos;
 
@@ -201,19 +204,53 @@ vector<Point2i> histograma(vector<Rect> boundRect, Mat frame){
         erode(rectInterested, rectInterested, getStructuringElement(MORPH_ELLIPSE, Size(5,5)));
         findContours(rectInterested, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
         if(contours.size()!=0){
-            rectInterestedVector.push_back(rectInterested);
-            rectangle(frame, boundRect[i], 100, 2, 8, 0);
-            punto=angulo(boundRect, i, frame);
-            puntos.push_back(punto);
+            amarillo = comp_Rect(amarillo, boundRect[i]);
+            //rectangle(frame, boundRect[i], 100, 2, 8, 0);
+
         }
+
+    }
+    for(int i=0; i<amarillo.size(); i++){
+        punto=angulo(amarillo[i], frame);
+        puntos.push_back(punto);
+        rectangle(frame, amarillo[i], 100, 2, 8, 0);
     }
     resize(frame, frameAux, Size(), 0.5, 0.5, INTER_LINEAR);
     imshow("Seccion por color", frameAux);
         return puntos;
 }
 
-Point2i angulo(vector<Rect> boundRect, int i, Mat frame){
-    Rect coord = boundRect.at(i);
+vector<Rect> comp_Rect(vector<Rect> amarillo, Rect boundRect){
+    if(amarillo.empty()==true){
+        amarillo.push_back(boundRect);
+    }
+    else{
+       /* int minX = boundRect.x;
+        int maxX = minX + boundRect.width;
+        int minY = boundRect.y;
+        int maxY = minY + boundRect.height;
+        int X2, Y2;
+        */
+        Rect comprobar;
+        Rect fusion;
+        for(int i=0; i<amarillo.size(); i++){
+            comprobar = amarillo.at(i);
+            //Si el nuevo Rect esta dentro del antiguo
+            //if(comprobar.x < minX && comprobar.x > maxX && comprobar.y < minY && comprobar.y > maxY){}
+            if((boundRect & comprobar).area()!=0){
+            fusion = boundRect | comprobar;
+            amarillo.at(i) = fusion;
+            }
+            else if(i == amarillo.size()-1){
+                amarillo.push_back(boundRect);
+            }
+        }
+    }
+    return amarillo;
+}
+
+
+Point2i angulo(Rect coord, Mat frame){
     int x = coord.x;
     int y = coord.y;
     Point2i punto;
@@ -268,3 +305,35 @@ while(true){
 }
       return;
 }
+
+
+vector<Point2i> hist_no_mov(Mat frame){
+
+    Mat frameAux;
+    Mat rectSection;
+    Mat rectSectionHSV;
+    Mat rectInterested;
+    vector<vector<Point> > contours;
+    vector<Rect> amarillo;
+    Point2i punto;
+    vector<Point2i> puntos;
+    vector<Rect> boundRect;
+
+        cvtColor(frame, rectSectionHSV, COLOR_BGR2HSV);
+        inRange(rectSectionHSV, Scalar(0,125,127), Scalar(255,255,255),rectInterested);
+        dilate(rectInterested,rectInterested, getStructuringElement(MORPH_ELLIPSE, Size(5,5)));
+        boundRect=detecta_contorno(rectInterested, frame);
+        for(int i=0; i<boundRect.size();i++){
+            amarillo = comp_Rect(amarillo, boundRect[i]);
+        }
+
+        for(int i=0; i<amarillo.size(); i++){
+            punto=angulo(amarillo[i], frame);
+            puntos.push_back(punto);
+            rectangle(frame, amarillo[i], 100, 2, 8, 0);
+        }
+    resize(frame, frameAux, Size(), 0.5, 0.5, INTER_LINEAR);
+    imshow("Seccion por color", frameAux);
+        return puntos;
+}
+
